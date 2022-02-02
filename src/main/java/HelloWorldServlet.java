0f9681b394d5130a3eb601c83e9cf5d6066eb5c8
michael galimore <michael.galimore1@gmail.com>
2022-02-02 12:29:34 -0600

    servlets-exercises

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,27 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.*;
 
+
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
+
+    int hitCounter;
+    public void init(){
+        hitCounter = 0;
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setContentType("text/html");
+        hitCounter++;
+        PrintWriter out = response.getWriter();
+        String title = "Times viewed ";
+        out.println("<h1>"+ title + "</h1>" + "<h2>" + hitCounter + "</h2>");
+
 
     }
 }
