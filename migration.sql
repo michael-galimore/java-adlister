df620c5045cb09949035cd56f9b40e7389a77ad4
michael galimore <michael.galimore1@gmail.com>
2022-02-10 15:41:01 -0600

    jdbc-exercise

--- migration.sql
+++ migration.sql
@@ -1,3 +1,25 @@
 USE adlister_db;
 
+DROP TABLE IF EXISTS ads;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL UNIQUE,
+    email VARCHAR(200) NOT NULL UNIQUE,
+    password VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ads(
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    title VARCHAR(200) NOT NULL UNIQUE,
+    description TEXT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY user(user_id) REFERENCES users(id)
+                ON DELETE CASCADE
+);
+
+
 
--- src/main/java/MySQLAdsDao.java
+++ src/main/java/MySQLAdsDao.java
@@ -1,39 +1,72 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
+
+
 import com.mysql.cj.jdbc.Driver;
 
 public class MySQLAdsDao implements Ads{
-    private Connection connection;
+    private Connection connection = null;
     private List<Ad> ads;
 
 
    public MySQLAdsDao(Config config){
        try {
            DriverManager.registerDriver(new Driver());
-       } catch (SQLException e) {
-           e.printStackTrace();
-       }
-       try {
-           Connection connection = DriverManager.getConnection(
+           connection = DriverManager.getConnection(
                    config.getUrl(),
                    config.getUser(),
-                   config.getPassword()
-           );
+                   config.getPassword());
        } catch (SQLException e) {
            e.printStackTrace();
        }
    }
+
     @Override
     public List<Ad> all() {
-        return null;
+       List<Ad> allAds = new ArrayList<>();
+        try{
+            Statement stmt = connection .createStatement();
+            String query = "SELECT * FROM ads";
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            while(rs.next()){
+                Ad newAd = new Ad(
+                        rs.getLong("id"),
+                        rs.getLong("user_id"),
+                        rs.getString("title"),
+                        rs.getString("description")
+                );
+                allAds.add(newAd);
+            }
+
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return allAds;
     }
 
     @Override
     public Long insert(Ad ad) {
-        return null;
+        try {
+            Statement stmt = connection.createStatement();
+            String insertQuery = "INSERT INTO ads(user_id, title, description) VALUES " +
+                    "(" + ad.getUserId() + ", " +
+                    "'" + ad.getTitle() + "', " +
+                    "'" + ad.getDescription() + "')";
+
+            stmt.executeUpdate(insertQuery, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+
+        } catch (SQLException e) {
+                throw new RuntimeException("Error creating ad.", e);
+            }
+        }
     }
 
 
-}
+
